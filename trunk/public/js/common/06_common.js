1f8d988066c2e95712dda11d65d1d96d7b2d6049
juanquedena@gmail.com <juanquedena@gmail.com@a9241c63-1b92-0d0a-44bc-011900d431f7>
2012-03-11 22:41:49 +0000

    Falta poco para terminar de armar la arquitectura.

--- trunk/public/js/common/06_common.js
+++ trunk/public/js/common/06_common.js
@@ -20,34 +20,31 @@ function openDialogConfirm1(contenido, width, height) {
     $('#jqDialogConfirmacion1').dialog('open');
 }
 
-function openDialogConfirm2(contenido) {
+function openDialogConfirm2(contenido, width, height) {
     $('#jqConfirmacion2').html(contenido);
     if(width != undefined) $('#jqDialogConfirmacion2').dialog('option', 'width', width);
     if(height != undefined) $('#jqDialogConfirmacion2').dialog('option', 'height', height);
     $('#jqDialogConfirmacion2').dialog('open');
 }
 
-function openDialogError(contenido) {
-    var contenido = '<p><span class=\"ui-icon ui-icon-alert\" style=\"float:left; margin:0 7px 7px 0;\"></span>';
-    contenido = contenido + msg;
-    contenido = contenido + '</p>';
-    $('#jqDialogError').html(contenido);
+function openDialogError(contenido, width, height) {
+    $('#jqError').html(contenido);
+    if(width != undefined) $('#jqDialogError').dialog('option', 'width', width);
+    if(height != undefined) $('#jqDialogError').dialog('option', 'height', height);
     $('#jqDialogError').dialog('open');
 }
 
-function openDialogWarning(contenido) {
-    var contenido = '<p><span class=\"ui-icon ui-icon-info\" style=\"float:left; margin:0 7px 7px 0;\"></span>';
-    contenido = contenido + msg;
-    contenido = contenido + '</p>';
-    $('#jqDialogWarning').html(contenido);
+function openDialogWarning(contenido, width, height) {
+    $('#jqWarning').html(contenido);
+    if(width != undefined) $('#jqDialogWarning').dialog('option', 'width', width);
+    if(height != undefined) $('#jqDialogWarning').dialog('option', 'height', height);
     $('#jqDialogWarning').dialog('open');
 }
 
-function openDialogInfo(contenido) {
-    var contenido = '<p><span class=\"ui-icon ui-icon-comment\" style=\"float:left; margin:0 7px 7px 0;\"></span>';
-    contenido = contenido + msg;
-    contenido = contenido + '</p>';
-    $('#jqDialogInfo').html(contenido);
+function openDialogInfo(contenido, width, height) {
+    $('#jqInfo').html(contenido);
+    if(width != undefined) $('#jqDialogInfo').dialog('option', 'width', width);
+    if(height != undefined) $('#jqDialogInfo').dialog('option', 'height', height);
     $('#jqDialogInfo').dialog('open');
 }
 
