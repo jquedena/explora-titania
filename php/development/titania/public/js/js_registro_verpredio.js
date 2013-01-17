df2bad011a1a6527f56f31e3dbf39209ee8e879f
juanquedena@gmail.com <juanquedena@gmail.com@a9241c63-1b92-0d0a-44bc-011900d431f7>
2013-01-17 19:56:53 +0000

    Titania

--- php/development/titania/public/js/js_registro_verpredio.js
+++ php/development/titania/public/js/js_registro_verpredio.js
@@ -182,6 +182,13 @@ loadVerDetalle = function(){
         // location.href = path + "registro";
     	window.open(pathReport+"reporte=Report_Completo&opt=p_mhresum^" + $("#_mhresum").val() + "|p_cperiod^-1|p_ccodpre^-1|p_mperson^" + $("#lblCodigo").html(), '_blank');
     });
+
+    $("#btnGaleria").button({
+        icons: {primary:'ui-icon-search'}
+    }).bind('click', function(){
+        // location.href = path + "registro";
+    	// window.open(pathReport+"reporte=Report_Completo&opt=p_mhresum^" + $("#_mhresum").val() + "|p_cperiod^-1|p_ccodpre^-1|p_mperson^" + $("#lblCodigo").html(), '_blank');
+    });
     
     actualizarGrid("tblPiso", optionPiso, null, function(){
         $("#tblPiso").jqGrid('navGrid', '#ptblPiso', {edit:false, add:false, del:false, search:false, refresh:false});
